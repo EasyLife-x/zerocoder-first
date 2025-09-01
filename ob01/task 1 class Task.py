23c061919a182c1e23c91bd962ea6f3038703642
EasyLife-x <easylife9525@gmail.com>
2025-09-01 13:02:21 +0300

    Update task 1 class Task.py

--- ob01/task 1 class Task.py
+++ ob01/task 1 class Task.py
@@ -1,23 +1,47 @@
 tasks = []
-
 def add_task():
     description = input("Введите описание задачи - ").strip()
     deadline = input("Срок выполнения DD/MM/YYYY - ").strip()
     status = False
     if description != '' and deadline != '':
         tasks.append(Task(description, deadline, status))
+    else:
+        print('Введите описание задачи и срок выполнения')
 
+def current_tasks():
+    current = []
+    print(f'\n Текущие задачи:')
+    for task in tasks:
+        if task.status == False:
+            current.append(task)
+            print(f'{task.description} - {task.deadline}')
+
+def complete(description):
+    task_to_complete = ''
+    for task in tasks:
+        if task.description == description: 
+            task.status = True
+            print(f'\n Задача {description} выполнена')
+            task_to_complete = task
+    if task_to_complete == '':
+        print('Задача не найдена')
 
 
 class Task():
-    def __init__(self, description, deadline, status):
+    def init(self, description, deadline, status):
         self.description = description
         self.deadline = deadline
         self.status = status
 
-    def complete(self):
-        self.status = True
+    def str(self):
+        return f'{self.description} - {self.deadline}'
 
 
 add_task()
-print(f'\n Список задач: {tasks}')
+add_task()
+add_task()
+
+current_tasks()
+
+complete(input('Введите описание задачи, которую хотите выполнить - '))
+current_tasks()
